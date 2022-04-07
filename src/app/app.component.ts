f496d3dc7ed7f3fccdcaf15996687cd9e76b5156
wojciech <wojciechchmura@gmail.com>
2022-04-07 20:24:16 +0200

    fetch todo on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDos } from './+state/todo.service';
+import { ToDoStatus } from './+state/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'WojciechChmura-Dataedo';
+export class AppComponent implements OnInit {
+  constructor(private toDosService: ToDos) {}
+
+  ngOnInit(): void {
+    this.fetchToDos();
+  }
+
+  fetchToDos(): void {
+    this.toDosService.getToDos().subscribe((toDos) => {
+      const secondPendingToDo = toDos.filter(
+        (toDo) => toDo.status === ToDoStatus.pending
+      )[1];
+      console.log(secondPendingToDo);
+    });
+  }
 }
